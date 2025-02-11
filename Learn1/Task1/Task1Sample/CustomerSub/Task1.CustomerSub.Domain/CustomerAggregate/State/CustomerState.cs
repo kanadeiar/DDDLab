a0c0a11eb9919eef1668e24f948d5db6c509f34a
kanadeiar <kanadeiar@gmail.com>
2025-02-12 01:42:55 +0300

    Выделение состояния от поведения

---
+++ Learn1/Task1/Task1Sample/CustomerSub/Task1.CustomerSub.Domain/CustomerAggregate/State/CustomerState.cs
@@ -0,0 +1,34 @@
+﻿using Task1.CustomerSub.Domain.Abstractions;
+using Task1.CustomerSub.Domain.CustomerAggregate.Events;
+using Task1.CustomerSub.Domain.CustomerAggregate.Values;
+
+namespace Task1.CustomerSub.Domain.CustomerAggregate.State;
+
+public class CustomerState
+{
+    public CustomerId Id { get; private set; }
+
+    public bool ConsumptionLocked { get; set; }
+
+    public bool ManualBilling { get; set; }
+
+    public void Apply(IEvent ev)
+    {
+        Mutate(ev);
+    }
+
+    public void Mutate(IEvent ev)
+    {
+        ((dynamic)this).When((dynamic)ev);
+    }
+
+    public void When(CustomerLocked e)
+    {
+        ConsumptionLocked = true;
+    }
+
+    public void When(CustomerUnlocked e)
+    {
+        ConsumptionLocked = true;
+    }
+}
